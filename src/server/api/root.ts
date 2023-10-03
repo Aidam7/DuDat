1a3d4305b3d8dfb18e4386078f573c8121763cd4
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-10-03 19:11:39 +0200

    [Progress] Group router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { tasksRouter } from "./routers/tasks";
+import { groupsRouter } from "./routers/group";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +8,8 @@ import { tasksRouter } from "./routers/tasks";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  tasks: tasksRouter
+  tasks: tasksRouter,
+  groups: groupsRouter
 });
 
 // export type definition of API
---
+++ src/server/api/routers/group.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const groupsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.group.findMany();
+  }),
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.prisma.group.findFirst({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+  create: publicProcedure
+    .input(z.object({ name: z.string(), memberIDs: z.array(z.string()) }))
+    .mutation(async ({ ctx, input }) => {
+        
+    }),
+});
