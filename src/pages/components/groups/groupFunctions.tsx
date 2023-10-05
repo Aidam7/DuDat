12845b64f2832c12a039b1914f3e755c705cc223
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-10-05 20:54:31 +0200

    [Fix] Potential promise fix

--- src/pages/components/groups/groupFunctions.tsx
+++ src/pages/components/groups/groupFunctions.tsx
@@ -3,13 +3,13 @@ import { api } from "~/utils/api";
 export const GroupFunctions: React.FC = () => {
   //! This is just a dev thing
   const deleteGroupMutation = api.groups.deleteById.useMutation();
-  const deleteGroup = async (id: string) => {
+  const deleteGroup = (id: string) => async () => {
     await deleteGroupMutation.mutateAsync({ id });
-  };
+  };  
   return (
     <>
       <h1>Groups</h1>
-      <button onClick={() => deleteGroup("")}>MAŽ</button>
+      <button onClick={deleteGroup("")}>MAŽ</button>
     </>
   );
 };
