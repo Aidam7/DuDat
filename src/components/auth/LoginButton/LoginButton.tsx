7cd1cc8740e33fa09a74c28e477f2098f5177b7b
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-01 11:38:41 +0100

    [Feature] Basic login button

--- src/components/auth/LoginButton/LoginButton.tsx
+++ src/components/auth/LoginButton/LoginButton.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@nextui-org/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
 
 const LoginButton: React.FC = () => {
-  return <></>;
+  const { data: sessionData } = useSession();
+  return (
+    <>
+      <p>
+        {sessionData && <span>{sessionData.user?.name} </span>}
+        <Button
+          color="primary"
+          onClick={sessionData ? () => void signOut() : () => void signIn()}
+        >
+          {sessionData ? "Sign out" : "Sign in"}
+        </Button>
+      </p>
+    </>
+  );
 };
 
 export default LoginButton;
