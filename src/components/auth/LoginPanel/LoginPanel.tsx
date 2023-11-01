6fad7ebca3537d4a81f584a633ff21adaef8536e
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-01 23:11:51 +0100

    [Addition] The navbar now has the users profile pic

--- src/components/auth/LoginPanel/LoginPanel.tsx
+++ src/components/auth/LoginPanel/LoginPanel.tsx
@@ -1,26 +1,37 @@
-import { Button } from "@nextui-org/react";
+import { Button, Image } from "@nextui-org/react";
 import { signIn, signOut, useSession } from "next-auth/react";
-import React from "react";
 
 const LoginButton: React.FC = () => {
   const { data: sessionData } = useSession();
   return (
-    <>
+    <div>
       <p>
-        {sessionData?.user.name && (
-          <span className="pr-5">
-            {sessionData.user.name.charAt(0).toUpperCase() +
-              sessionData.user.name.slice(1)}
-          </span>
-        )}
-        <Button
-          color="primary"
-          onClick={sessionData ? () => void signOut() : () => void signIn()}
-        >
-          {sessionData ? "Sign out" : "Sign in"}
-        </Button>
+        <div className="flex items-center">
+          {sessionData?.user.image && (
+            <Image
+              src={sessionData.user.image}
+              alt="Users avatar"
+              width={32}
+              height={32}
+              className="mr-2 rounded-full"
+            />
+          )}
+          {sessionData?.user.name && (
+            <span className="pl-2.5 pr-5">
+              {sessionData.user.name.charAt(0).toUpperCase() +
+                sessionData.user.name.slice(1)}
+            </span>
+          )}
+
+          <Button
+            color="primary"
+            onClick={sessionData ? () => void signOut() : () => void signIn()}
+          >
+            {sessionData ? "Sign out" : "Sign in"}
+          </Button>
+        </div>
       </p>
-    </>
+    </div>
   );
 };
 
