34571e8bf8b21bbb89d458de17ea2e240b933577
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-02 13:36:10 +0100

    [Progress] Minor changes to LoginPanel Adjusted padding around name The name and avatar are now a link Removed unnecessary div

--- src/components/auth/LoginPanel/LoginPanel.tsx
+++ src/components/auth/LoginPanel/LoginPanel.tsx
@@ -1,4 +1,4 @@
-import { Button, Image } from "@nextui-org/react";
+import { Button, Image, Link } from "@nextui-org/react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 
@@ -15,25 +15,26 @@ const LoginButton: React.FC = () => {
   }
 
   return (
-    <div>
+    <>
       <p>
         <div className="flex items-center">
-          {sessionData?.user.image && (
-            <Image
-              src={sessionData.user.image}
-              alt="Users avatar"
-              width={32}
-              height={32}
-              className="mr-2 rounded-full"
-            />
-          )}
-          {sessionData?.user.name && (
-            <span className="pl-2.5 pr-5">
-              {sessionData.user.name.charAt(0).toUpperCase() +
-                sessionData.user.name.slice(1)}
-            </span>
-          )}
-
+          <Link href="#" className="text-foreground">
+            {sessionData?.user.image && (
+              <Image
+                src={sessionData.user.image}
+                alt="Users avatar"
+                width={48}
+                height={48}
+                className="rounded-full"
+              />
+            )}
+            {sessionData?.user.name && (
+              <span className="pl-2 pr-4">
+                {sessionData.user.name.charAt(0).toUpperCase() +
+                  sessionData.user.name.slice(1)}
+              </span>
+            )}
+          </Link>
           <Button
             color="primary"
             onClick={sessionData ? () => void signOut() : () => void signIn()}
@@ -42,7 +43,7 @@ const LoginButton: React.FC = () => {
           </Button>
         </div>
       </p>
-    </div>
+    </>
   );
 };
 
