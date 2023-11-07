fa3022318a40d5b61185175e2c7ba84bfac0bce1
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-07 10:29:31 +0100

    [Rework] Cat

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+import Image from 'next/image';
+import img404 from 'public/images/404.jpg';
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-9xl mb-10 hidden">404</h1>
+      <p className="text-3xl mb-10 hidden">Sorry, the page you are looking for does not exist.</p>
+      <Image src={img404} alt="404" width={500} height={500} />
     </div>
   );
 };
