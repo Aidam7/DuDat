20d1ced1a89c0bf2283f4ee3f0c58eddb2f753bb
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-14 10:32:49 +0100

    [Progress] Currently does not compile

--- src/pages/groups/[id].tsx
+++ src/pages/groups/[id].tsx
@@ -3,6 +3,9 @@ import { api } from "~/utils/api";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import Code404 from "~/components/layout/errorCodes/404";
+import { Button } from "@nextui-org/react";
+import TaskTable from "~/components/tasks/table";
+import { ITableColumns } from "~/utils/types";
 
 export default function GroupDetail() {
   const router = useRouter();
@@ -16,12 +19,28 @@ export default function GroupDetail() {
       onSuccess: () => setLoading(false),
     },
   );
+  const { data: isMemberOfGroup } = api.users.isMemberOfGroup.useQuery({
+    groupId: id,
+    userId: session?.user.id,
+  });
   if (!session) return <>Please sign in</>;
   if (loading) return <>Loading...</>;
   if (!group) return <Code404 />;
+  const columns: ITableColumns[] = [
+    { key: "title", label: "Title" },
+    { key: "description", label: "Description" },
+  ];
   return (
     <>
       <h1 className="text-6xl">{group.name}</h1>
+      <Button
+        color="primary"
+        className="mb-5 ml-auto w-min"
+        onClick={() => router.push(`/tasks/create?groupId=${id}`)}
+      >
+        Create a new task
+      </Button>
+      <TaskTable />
     </>
   );
 }
