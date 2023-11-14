c3650d2fce2d2076425f03df98ab3bdc25d28c2d
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-14 21:00:32 +0100

    [Feature] Basic group edit form

--- src/components/groups/groupEdit/groupEdit.tsx
+++ src/components/groups/groupEdit/groupEdit.tsx
@@ -1,26 +1,42 @@
 import { Input } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { type FC, useState, type FormEvent } from "react";
+import { type FC, useState, type FormEvent, useEffect } from "react";
+import Code404 from "~/components/layout/errorCodes/404";
 import { api } from "~/utils/api";
-
-export const GroupCreate: FC = () => {
+interface Props {
+  groupId: string;
+}
+export const GroupEdit: FC<Props> = (props: Props) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const createGroupMutation = api.groups.edit.useMutation();
+  const editGroupMutation = api.groups.edit.useMutation();
   const { data: session } = useSession();
   const router = useRouter();
-
+  const { data: group } = api.groups.getById.useQuery(
+    { id: props.groupId },
+    { enabled: session !== null },
+  );
+  useEffect(() => {
+    if (group) {
+      setName(group.name);
+      setDescription(group.description);
+    }
+  }, [group]);
+  if (!group) return <Code404 />;
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     //*This is the only way I could find that prevents a redirect on the same page
     event.preventDefault();
     if (!session) return null;
-    const group = await createGroupMutation.mutateAsync({
-      name,
-      description,
-      ownerId: session.user.id,
-      groupId: "",
-    });
+    await editGroupMutation.mutateAsync(
+      {
+        name,
+        description,
+        ownerId: session.user.id,
+        groupId: group.id,
+      },
+      { onSuccess: () => router.push(`/groups/${group.id}`) },
+    );
   };
 
   return (
@@ -29,7 +45,7 @@ export const GroupCreate: FC = () => {
         className="flex flex-col items-center justify-center"
         onSubmit={handleSubmit}
       >
-        <h3 className="mb-4 text-3xl font-bold">Create a group</h3>
+        <h3 className="mb-4 text-3xl font-bold">Edit a group</h3>
         <Input
           type="text"
           label="Group name"
@@ -45,7 +61,6 @@ export const GroupCreate: FC = () => {
           onValueChange={setDescription}
           className="mb-4"
         />
-
         <Input type="submit" value="Submit" color="primary" />
       </form>
     </>
