fda5f1b7fde442217250ee24f96ac444d34beadf
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-15 11:08:27 +0100

    [Progress] Mini refactor

--- src/pages/groups/[id]/admin.tsx
+++ src/pages/groups/[id]/admin.tsx
@@ -4,33 +4,35 @@ import { useState } from "react";
 import { useSession } from "next-auth/react";
 import Code404 from "~/components/layout/errorCodes/404";
 import Code401 from "~/components/layout/errorCodes/401";
+import { GroupEdit } from "~/components/groups/groupEdit/groupEdit";
 
 export default function GroupDetail() {
   const router = useRouter();
-  const id = router.query.id as string;
+  const groupId = router.query.id as string;
   const [loading, setLoading] = useState(true);
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const [authLoading, setAuthLoading] = useState(true);
   const isOwnerOfGroupQuery = api.users.isOwnerOfGroup;
   const { data: group } = api.groups.getById.useQuery(
-    { id },
+    { id: groupId },
     {
       enabled: session != null,
       onSuccess: () => setLoading(false),
     },
   );
-  let memberId = "";
-  if (session) memberId = session.user.id;
+  let userId = "";
+  if (session) userId = session.user.id;
   const { data: isOwner } = isOwnerOfGroupQuery.useQuery(
     {
-      groupId: id,
-      userId: memberId,
+      groupId,
+      userId,
     },
     {
       enabled: session != null && group != null,
       onSuccess: () => setAuthLoading(false),
     },
   );
+  if (status === "loading") return <>Loading...</>;
   if (!session) return <>Please sign in</>;
   if (loading) return <>Loading...</>;
   if (!group) return <Code404 />;
@@ -38,7 +40,8 @@ export default function GroupDetail() {
   if (!isOwner) return <Code401 />;
   return (
     <>
-      <h1 className="text-6xl">Admin: {group.name}</h1>
+      <h1 className="text-6xl">{group.name} settings</h1>
+      <GroupEdit groupId={groupId} />
     </>
   );
 }
