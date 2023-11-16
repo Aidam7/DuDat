e8e38e55b9fa7db4b93c829a10f988e7f81b7343
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-16 13:20:33 +0100

    [Feature] Added delete button

--- src/pages/groups/[id]/admin.tsx
+++ src/pages/groups/[id]/admin.tsx
@@ -1,6 +1,7 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import GroupDelete from "~/components/groups/groupDelete";
 import { GroupEdit } from "~/components/groups/groupEdit/groupEdit";
 import Code401 from "~/components/layout/errorCodes/401";
 import Code404 from "~/components/layout/errorCodes/404";
@@ -41,8 +42,13 @@ export default function GroupDetail() {
   return (
     <>
       <h1 className="text-6xl">{group.name} settings</h1>
-      <div className="w-[25%]">
-        <GroupEdit groupId={groupId} />
+      <div className="w-full sm:w-[25%]">
+        <div className="mb-5">
+          <GroupEdit groupId={groupId} />
+        </div>
+        <div className="sw:w-[25%] w-full">
+          <GroupDelete groupId={groupId} />
+        </div>
       </div>
     </>
   );
