0b0242a074dd7dd036b04b2dfd094d5ac7af0311
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-21 23:07:42 +0100

    [Progress] split index to index and admin panel

--- src/pages/groups/[groupId]/tasks/[taskId]/admin.tsx
+++ src/pages/groups/[groupId]/tasks/[taskId]/admin.tsx
@@ -2,6 +2,8 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Code401 from "~/components/layout/errorCodes/401";
 import Code404 from "~/components/layout/errorCodes/404";
+import TaskAddAssignments from "~/components/tasks/taskAddAssignments";
+import TaskRemoveAssignments from "~/components/tasks/taskRemoveAssignments";
 import { api } from "~/utils/api";
 export default function TaskDetail() {
   const router = useRouter();
@@ -20,7 +22,7 @@ export default function TaskDetail() {
   const { data: group } = api.groups.getById.useQuery({ id: groupId });
   if (!session) return <>Please sign in</>;
   if (authenticating) return <>Authenticating...</>;
-  if (!isMember) return <Code401 />;
+  if (!isMember || task?.authorId != session.user.id) return <Code401 />;
   if (loading) return <>Loading...</>;
   if (!task || !group) return <Code404 />;
   return (
@@ -29,8 +31,12 @@ export default function TaskDetail() {
       {task.description}
       <br></br>
       {task.authorId}
+      <br />
+      {session.user.id}
       <br></br>
       {task.title}
+      <TaskAddAssignments group={group} task={task} />
+      <TaskRemoveAssignments group={group} task={task} />
     </>
   );
 }
