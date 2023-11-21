9da41b2df94a9bb4a48d98dca114b00c96bba444
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-21 10:19:31 +0100

    [Feature] Secured tasks

--- src/pages/groups/[groupId]/tasks/[taskId].tsx
+++ src/pages/groups/[groupId]/tasks/[taskId].tsx
@@ -1,5 +1,6 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import Code401 from "~/components/layout/errorCodes/401";
 import Code404 from "~/components/layout/errorCodes/404";
 import { api } from "~/utils/api";
 export default function TaskDetail() {
@@ -7,12 +8,18 @@ export default function TaskDetail() {
   const taskId = router.query.taskId as string;
   const groupId = router.query.groupId as string;
   const { data: session } = useSession();
-
+  const { data: isMember, isFetching: authenticating } =
+    api.users.isMemberOfGroup.useQuery(
+      { groupId: groupId, userId: session?.user?.id ?? "" },
+      { enabled: session != null },
+    );
   const { data: task, isFetching: loading } = api.tasks.getById.useQuery(
     { id: taskId },
-    { enabled: session != null },
+    { enabled: session != null && isMember },
   );
   if (!session) return <>Please sign in</>;
+  if (authenticating) return <>Authenticating...</>;
+  if (!isMember) return <Code401 />;
   if (loading) return <>Loading...</>;
   if (!task) return <Code404 />;
   return (
--- src/pages/groups/[groupId]/tasks/create.tsx
+++ src/pages/groups/[groupId]/tasks/create.tsx
@@ -1,12 +1,22 @@
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
-import Code400 from "~/components/layout/errorCodes/400";
+import Code404 from "~/components/layout/errorCodes/404";
 import TaskCreateForm from "~/components/tasks/taskCreate";
+import { api } from "~/utils/api";
 
 const Create: React.FC = () => {
   const router = useRouter();
+  const { data: session } = useSession();
   const groupId = router.query.groupId as string;
-  if (!groupId) return <Code400 specifier="Invalid group ID" />;
+  const { data: isMember, isFetching: authenticating } =
+    api.users.isMemberOfGroup.useQuery(
+      { groupId: groupId, userId: session?.user?.id ?? "" },
+      { enabled: session != null },
+    );
+  if (!session) return <>Please sign in</>;
+  if (authenticating) return <>Authenticating...</>;
+  if (!isMember && !authenticating) return <Code404 />;
   return (
     <>
       <TaskCreateForm groupId={groupId} />
