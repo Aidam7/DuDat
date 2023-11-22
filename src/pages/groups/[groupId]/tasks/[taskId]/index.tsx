04eba37cac0bc187841981346b620c1ef62e0a4f
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-22 12:09:45 +0100

    [Progress] Skeleton page

--- src/pages/groups/[groupId]/tasks/[taskId]/index.tsx
+++ src/pages/groups/[groupId]/tasks/[taskId]/index.tsx
@@ -2,6 +2,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Code401 from "~/components/layout/errorCodes/401";
 import Code404 from "~/components/layout/errorCodes/404";
+import UserTable from "~/components/users/table";
 import { api } from "~/utils/api";
 export default function TaskDetail() {
   const router = useRouter();
@@ -18,6 +19,19 @@ export default function TaskDetail() {
     { enabled: session != null && isMember },
   );
   const { data: group } = api.groups.getById.useQuery({ id: groupId });
+  const { data: assignees, isFetching: loadingAssignees } =
+    api.tasks.getAssignedMembers.useQuery(
+      {
+        taskId: taskId,
+        groupId: groupId,
+      },
+      {
+        enabled:
+          (task != null || task != undefined) &&
+          isMember &&
+          (group != null || group != undefined),
+      },
+    );
   if (!session) return <>Please sign in</>;
   if (authenticating) return <>Authenticating...</>;
   if (!isMember) return <Code401 />;
@@ -25,12 +39,20 @@ export default function TaskDetail() {
   if (!task || !group) return <Code404 />;
   return (
     <>
-      <h1>{task.id}</h1>
-      {task.description}
-      <br></br>
-      {task.authorId}
-      <br></br>
-      {task.title}
+      <h1 className="text-6xl">{task.title}</h1>
+      {task.description != "" ? (
+        <span>{task.description}</span>
+      ) : (
+        <span className="italic text-gray-600">
+          No description was provided
+        </span>
+      )}
+      <h2 className="text-4xl">Assignees</h2>
+      {assignees ? (
+        <UserTable rows={assignees} loading={loadingAssignees} />
+      ) : (
+        <UserTable rows={[]} loading={loadingAssignees} />
+      )}
     </>
   );
 }
