895b1a5f44137bf0abbb7966857aa1092a2146fd
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-22 20:05:15 +0100

    [Feature] Something something Links something something

--- src/components/tasks/table/table.tsx
+++ src/components/tasks/table/table.tsx
@@ -8,6 +8,7 @@ import {
   TableRow,
 } from "@nextui-org/react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { type FC } from "react";
 import { type ITaskWithGroup } from "~/utils/types";
 
@@ -15,43 +16,75 @@ type Props = {
   rows: ITaskWithGroup[];
   loading: boolean;
   doNotRenderGroup?: boolean;
+  link?: string;
 };
+
 export const TaskTable: FC<Props> = (props: Props) => {
-  return (
-    <Table isStriped selectionMode="single">
-      <TableHeader>
-        <TableColumn>Title</TableColumn>
-        <TableColumn>Description</TableColumn>
-        <TableColumn>Due Date</TableColumn>
-        <TableColumn className={props.doNotRenderGroup ? "hidden" : ""}>
-          Group
-        </TableColumn>
-      </TableHeader>
-      <TableBody
-        items={props.rows}
-        isLoading={props.loading}
-        loadingContent={<Spinner label="Loading..." />}
-        emptyContent={"We couldn't find anything"}
-      >
-        {(task) => (
-          <TableRow>
+  const router = useRouter();
+  const tableHeader = (
+    <TableHeader>
+      <TableColumn>Title</TableColumn>
+      <TableColumn>Description</TableColumn>
+      <TableColumn>Due Date</TableColumn>
+      <TableColumn className={props.doNotRenderGroup ? "hidden" : ""}>
+        Group
+      </TableColumn>
+    </TableHeader>
+  );
+
+  const tableBody = (
+    <TableBody
+      items={props.rows}
+      isLoading={props.loading}
+      loadingContent={<Spinner label="Loading..." />}
+      emptyContent={"We couldn't find anything"}
+    >
+      {(task) => (
+        <TableRow className="group">
+          {props.link ? (
+            <TableCell>{task.title}</TableCell>
+          ) : (
             <TableCell>
-              <Link href={`/groups/${task.groupId}/tasks/${task.id}`}>
+              <Link
+                href={`groups/${task.groupId}/tasks/${task.id}`}
+                className="paren group-hover:underline"
+              >
                 {task.title}
               </Link>
             </TableCell>
-            <TableCell>
-              {task.description != "" ? task.description : "—"}
-            </TableCell>
-            <TableCell>
-              {task.dueOn != null ? task.dueOn.toLocaleDateString() : "—"}
-            </TableCell>
-            <TableCell className={props.doNotRenderGroup ? "hidden" : ""}>
-              <Link href={`/groups/${task.groupId}`}>{task.group.name}</Link>
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+          )}
+
+          <TableCell>
+            {task.description !== "" ? task.description : "—"}
+          </TableCell>
+          <TableCell>
+            {task.dueOn !== null ? task.dueOn.toLocaleDateString() : "—"}
+          </TableCell>
+          <TableCell className={props.doNotRenderGroup ? "hidden" : ""}>
+            <Link href={`/groups/${task.groupId}`}>{task.group.name}</Link>
+          </TableCell>
+        </TableRow>
+      )}
+    </TableBody>
+  );
+
+  return (
+    <>
+      {props.link ? (
+        <Table
+          isStriped
+          selectionMode="single"
+          onRowAction={(key) => router.push(`${props.link}${key}`)}
+        >
+          {tableHeader}
+          {tableBody}
+        </Table>
+      ) : (
+        <Table isStriped selectionMode="single">
+          {tableHeader}
+          {tableBody}
+        </Table>
+      )}
+    </>
   );
 };
