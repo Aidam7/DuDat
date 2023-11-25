20fc4972ab70f79012fb01f5248d79fb1ea25e91
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-25 16:41:55 +0100

    [Feature] You can now self assign and unassign

--- src/pages/groups/[groupId]/tasks/[taskId]/index.tsx
+++ src/pages/groups/[groupId]/tasks/[taskId]/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Code401 from "~/components/layout/errorCodes/401";
@@ -32,11 +33,44 @@ export default function TaskDetail() {
           (group != null || group != undefined),
       },
     );
+  const assignToTaskMutation = api.tasks.assignUser.useMutation();
+  const unassignFromTaskMutation = api.tasks.unassignUser.useMutation();
+  const apiUtils = api.useUtils();
   if (!session) return <>Please sign in</>;
   if (authenticating) return <>Authenticating...</>;
   if (!isMember) return <Code401 />;
   if (loading) return <>Loading...</>;
   if (!task || !group) return <Code404 />;
+  const isAuthor = task.authorId === session.user.id;
+  const isAssigned = assignees?.some((u) => u.id === session.user.id);
+  function handleAssignToTask() {
+    if (!session || !task) return;
+    return assignToTaskMutation.mutate(
+      {
+        taskId: task.id,
+        userId: session.user.id,
+      },
+      {
+        onSuccess: () => {
+          void apiUtils.tasks.getAssignedMembers.invalidate();
+        },
+      },
+    );
+  }
+  function handleUnassignFromTask() {
+    if (!session || !task) return;
+    return unassignFromTaskMutation.mutate(
+      {
+        taskId: task.id,
+        userId: session.user.id,
+      },
+      {
+        onSuccess: () => {
+          void apiUtils.tasks.getAssignedMembers.invalidate();
+        },
+      },
+    );
+  }
   return (
     <>
       <h1 className="text-6xl">{task.title}</h1>
@@ -47,6 +81,31 @@ export default function TaskDetail() {
           No description was provided
         </span>
       )}
+      <div className="ml-auto flex flex-col">
+        {isAssigned ? (
+          <Button
+            color="danger"
+            className="mb-2"
+            onPress={handleUnassignFromTask}
+          >
+            Unassign from task
+          </Button>
+        ) : (
+          <Button color="primary" className="mb-2" onPress={handleAssignToTask}>
+            Assign to task
+          </Button>
+        )}
+        {isAuthor && (
+          <Button
+            color="warning"
+            onClick={() =>
+              router.push(`/groups/${groupId}/tasks/${taskId}/admin`)
+            }
+          >
+            Settings
+          </Button>
+        )}
+      </div>
       <h2 className="text-4xl">Assignees</h2>
       {assignees ? (
         <UserTable rows={assignees} loading={loadingAssignees} />
