cbc983722a1e8ae7a426b4c2b7539af21bce13f2
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-11-26 18:37:37 +0100

    [Feature] Finished tasks are now separated

--- src/pages/groups/[groupId]/index.tsx
+++ src/pages/groups/[groupId]/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import Code401 from "~/components/layout/errorCodes/401";
 import Code404 from "~/components/layout/errorCodes/404";
 import TaskTable from "~/components/tasks/table";
@@ -14,6 +15,7 @@ export default function GroupDetail() {
   const isMemberOfGroupQuery = api.users.isMemberOfGroup;
   const isOwnerOfGroupQuery = api.users.isOwnerOfGroup;
   const findTasks = api.tasks.locateByName;
+  const [finishedTasksOpen, setFinishedTasksOpen] = useState(false);
   const { data: group, isFetching: loading } = api.groups.getById.useQuery(
     { id: groupId },
     {
@@ -65,10 +67,13 @@ export default function GroupDetail() {
     return <>Authenticating...</>;
   if (!isMember) return <Code401 />;
   const wishes = tasksAndWishes?.filter(
-    (task) => task.taskAssignment.length == 0,
+    (task) => task.taskAssignment.length == 0 && task.finishedOn == null,
+  );
+  const finishedTasks = tasksAndWishes?.filter(
+    (task) => task.finishedOn != null,
   );
   const tasks = tasksAndWishes?.filter(
-    (task) => task.taskAssignment.length > 0,
+    (task) => task.taskAssignment.length > 0 && task.finishedOn == null,
   );
   return (
     <>
@@ -128,11 +133,39 @@ export default function GroupDetail() {
           )}
         </div>
       </div>
-      <h2 className="pb-5 text-4xl">Members</h2>
-      {members ? (
-        <UserTable loading={loadingMembers} rows={members} />
-      ) : (
-        <UserTable loading={loadingMembers} rows={[]} />
+      <div className="pb-5">
+        <h2 className="pb-5 text-4xl">Members</h2>
+        {members ? (
+          <UserTable loading={loadingMembers} rows={members} />
+        ) : (
+          <UserTable loading={loadingMembers} rows={[]} />
+        )}
+      </div>
+      <Button
+        onClick={() => setFinishedTasksOpen(!finishedTasksOpen)}
+        color="primary"
+        className="w-1/4 max-md:w-full"
+      >
+        {finishedTasksOpen ? "▲ Close" : "▼ Open Finished Tasks"}
+      </Button>
+      {finishedTasksOpen && (
+        <div className="pt-5">
+          {finishedTasks ? (
+            <TaskTable
+              loading={loading}
+              rows={finishedTasks}
+              doNotRenderGroup
+              link={`/groups/${groupId}/tasks/`}
+            />
+          ) : (
+            <TaskTable
+              loading={loading}
+              rows={[]}
+              doNotRenderGroup
+              link={`/groups/${groupId}/tasks/`}
+            />
+          )}
+        </div>
       )}
     </>
   );
