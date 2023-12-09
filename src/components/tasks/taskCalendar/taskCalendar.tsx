92ee47597134acf1fdcf61ae02d0d1d9eda8914c
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-12-09 19:52:58 +0100

    [Feature] Now redirects to task

--- src/components/tasks/taskCalendar/taskCalendar.tsx
+++ src/components/tasks/taskCalendar/taskCalendar.tsx
@@ -1,6 +1,7 @@
 import { type Task } from "@prisma/client";
 import dayjs from "dayjs";
-import { type FC } from "react";
+import { useRouter } from "next/navigation";
+import { useCallback, type FC } from "react";
 import { Calendar, dayjsLocalizer } from "react-big-calendar";
 interface Props {
   tasks: Task[];
@@ -12,9 +13,17 @@ const TaskCalendar: FC<Props> = (props: Props) => {
       start: new Date(task.startOn ? task.startOn : task.createdOn),
       end: new Date(task.dueOn ? task.dueOn : task.createdOn),
       title: task.title,
+      taskId: task.id,
+      groupId: task.groupId,
     }));
-
+  const router = useRouter();
   const localizer = dayjsLocalizer(dayjs);
+  const onSelectEvent = useCallback(
+    (task: { taskId: string; groupId: string }) => {
+      router.push(`/groups/${task.groupId}/tasks/${task.taskId}`);
+    },
+    [router],
+  );
   return (
     <div className="flex flex-col items-center rounded-xl bg-white">
       <Calendar
@@ -23,6 +32,7 @@ const TaskCalendar: FC<Props> = (props: Props) => {
         endAccessor="end"
         events={events}
         className="min-h-[600px] w-[97.5%] bg-white text-black max-md:w-[90%]"
+        onSelectEvent={onSelectEvent}
       />
     </div>
   );
