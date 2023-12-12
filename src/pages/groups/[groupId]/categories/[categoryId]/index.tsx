a54895473cf484c9ee72d7826a11d3ead43f4ec6
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-12-12 10:33:16 +0100

    [Feature] Category admin panel

--- src/pages/groups/[groupId]/categories/[categoryId]/index.tsx
+++ src/pages/groups/[groupId]/categories/[categoryId]/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@nextui-org/react";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Code404 from "~/components/layout/errorCodes/404";
 import TaskTable from "~/components/tasks/table";
@@ -5,14 +7,19 @@ import { api } from "~/utils/api";
 
 export default function CategoryDetail() {
   const router = useRouter();
+  const { data: session, status } = useSession();
   const categoryId = router.query.categoryId as string;
   const groupId = router.query.groupId as string;
   const { data: category, isFetching: loading } =
     api.categories.getById.useQuery({ id: categoryId });
   const { data: tasks, isFetching: loadingTasks } =
     api.categories.getTasks.useQuery({ id: categoryId });
-  if (loading) return <div>Loading...</div>;
+  if (loading || status == "loading") return <div>Loading...</div>;
+  if (!session) return <>Please sign in</>;
   if (!category) return <Code404 />;
+  const isAuthor =
+    category.authorId == session.user.id ||
+    category.group.ownerId == session.user.id;
   return (
     <>
       <h1 className="text-6xl">{category.name}</h1>
@@ -23,6 +30,18 @@ export default function CategoryDetail() {
           No description was provided
         </span>
       )}
+      <div className="flex-co ml-auto flex gap-2">
+        {isAuthor && (
+          <Button
+            color="warning"
+            onClick={() =>
+              router.push(`/groups/${groupId}/categories/${categoryId}/admin`)
+            }
+          >
+            Settings
+          </Button>
+        )}
+      </div>
       <h2 className="pb-5 text-4xl">Tasks</h2>
       {tasks ? (
         <TaskTable
