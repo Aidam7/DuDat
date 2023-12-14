18df84641f064c4f96963f984117d39a1c767673
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-12-14 18:23:36 +0100

    [Feature] Added member check

--- src/pages/groups/[groupId]/categories/create.tsx
+++ src/pages/groups/[groupId]/categories/create.tsx
@@ -1,6 +1,25 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import CategoryCreate from "~/components/categories/categoryCreate";
+import Code401 from "~/components/layout/errorCodes/401";
+import { api } from "~/utils/api";
 
 function CreateCategory() {
+  const router = useRouter();
+  const groupId = router.query.groupId as string;
+  const { data: session, status } = useSession();
+  const { data: isMember, isFetching: loading } =
+    api.users.isMemberOfGroup.useQuery(
+      {
+        groupId: groupId,
+        userId: session?.user ? session.user.id : "",
+      },
+      {
+        enabled: session?.user != null,
+      },
+    );
+  if (status === "loading" || loading) return <div>Loading...</div>;
+  if (!isMember) return <Code401 />;
   return <CategoryCreate />;
 }
 
