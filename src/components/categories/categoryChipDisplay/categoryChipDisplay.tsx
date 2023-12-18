b9a5054fe18502a629532f03a35fd35e24f94002
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-12-18 13:21:48 +0100

    [Cleanup] Removed loading spinner

--- src/components/categories/categoryChipDisplay/categoryChipDisplay.tsx
+++ src/components/categories/categoryChipDisplay/categoryChipDisplay.tsx
@@ -1,4 +1,4 @@
-import { Chip, Spinner } from "@nextui-org/react";
+import { Chip } from "@nextui-org/react";
 import { type Category } from "@prisma/client";
 import Link from "next/link";
 import { type FC } from "react";
@@ -7,7 +7,6 @@ interface Props {
   loading?: boolean;
 }
 const CategoryChipDisplay: FC<Props> = (props: Props) => {
-  if (props.loading) return <Spinner label="Loading..." />;
   if (!props.categories) return null;
   return (
     <>
