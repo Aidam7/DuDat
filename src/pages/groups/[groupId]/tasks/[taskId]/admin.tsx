c1a46bd1a08ede9abfe1b91364c596ad5d2f31e9
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-12-21 13:18:17 +0100

    [Feature] Now allows group owners

--- src/pages/groups/[groupId]/tasks/[taskId]/admin.tsx
+++ src/pages/groups/[groupId]/tasks/[taskId]/admin.tsx
@@ -27,7 +27,12 @@ export default function TaskAdminPanel() {
     api.tasks.confirmTaskAsFinished.useMutation();
   if (!session) return <>Please sign in</>;
   if (authenticating) return <>Authenticating...</>;
-  if (!isMember || task?.authorId != session.user.id) return <Code401 />;
+  if (
+    !isMember ||
+    (task?.authorId != session.user.id &&
+      task?.group.ownerId != session.user.id)
+  )
+    return <Code401 />;
   if (loading) return <>Loading...</>;
   if (!task || !group) return <Code404 />;
   function handleConfirmTaskAsFinished() {
