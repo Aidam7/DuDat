a8a365c9a40e0d37309567483a355e5762c29f32
Aidam <55688345+Aidam7@users.noreply.github.com>
2024-01-17 16:56:23 +0100

    [Fix] Removed onSuccess callback

--- src/pages/groups/[groupId]/admin.tsx
+++ src/pages/groups/[groupId]/admin.tsx
@@ -11,21 +11,18 @@ import Code404 from "~/components/layout/errorCodes/404";
 import { api } from "~/utils/api";
 
 export default function GroupAdminPanel() {
-  //TODO: Remove onSuccess callbacks (issue #19)
   const router = useRouter();
   const groupId = router.query.groupId as string;
-  const [loading, setLoading] = useState(true);
+
   const { data: session, status } = useSession();
-  const { data: group } = api.groups.getById.useQuery(
+  const { data: group, isFetching: loading } = api.groups.getById.useQuery(
     { id: groupId },
     {
       enabled: session != null,
-      onSuccess: () => setLoading(false),
     },
   );
-  if (status === "loading") return <>Loading...</>;
+  if (status === "loading" || loading) return <>Loading...</>;
   if (!session) return <>Please sign in</>;
-  if (loading) return <>Loading...</>;
   if (!group) return <Code404 />;
   if (session.user.id != group.ownerId) return <Code401 />;
   return (
