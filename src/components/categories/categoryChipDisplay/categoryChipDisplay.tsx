07cf77a1444e7fe4a63cae828dd45dfff74151d7
Aidam <55688345+Aidam7@users.noreply.github.com>
2024-02-06 08:36:44 +0100

    [Rework] Minor rework

--- src/components/categories/categoryChipDisplay/categoryChipDisplay.tsx
+++ src/components/categories/categoryChipDisplay/categoryChipDisplay.tsx
@@ -3,21 +3,32 @@ import { type Category } from "@prisma/client";
 import Link from "next/link";
 import { type FC } from "react";
 interface Props {
-  categories: Category[] | undefined;
+  categories: Category[] | undefined | null;
   loading?: boolean;
+  displayHeader?: boolean;
+  displayWrapper?: boolean;
 }
 const CategoryChipDisplay: FC<Props> = (props: Props) => {
-  if (!props.categories) return null;
+  if (!props.categories || props.categories.length == 0) return null;
   return (
     <>
-      {props.categories.map((category) => (
-        <Link
-          key={category.id}
-          href={`/groups/${category.groupId}/categories/${category.id}`}
-        >
-          <Chip variant="dot">{category.name}</Chip>
-        </Link>
-      ))}
+      {props.displayHeader && <h2 className="pb-5 text-4xl">Categories</h2>}
+      <div
+        className={
+          props.displayWrapper
+            ? "flex flex-wrap gap-4 overflow-auto rounded-2xl bg-content1 p-4 shadow-small"
+            : ""
+        }
+      >
+        {props.categories.map((category) => (
+          <Link
+            key={category.id}
+            href={`/groups/${category.groupId}/categories/${category.id}`}
+          >
+            <Chip variant="dot">{category.name}</Chip>
+          </Link>
+        ))}
+      </div>
     </>
   );
 };
