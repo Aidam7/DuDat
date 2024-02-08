95c60fac86afb15d2176986fa990fb25c076e8f4
Aidam <55688345+Aidam7@users.noreply.github.com>
2024-02-08 12:59:08 +0100

    [Feature] Added generic page header

---
+++ src/components/layout/pageHeader/pageHeader.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type Props = {
+  name: string;
+  description: string;
+};
+
+const PageHeader: React.FC<Props> = (props: Props) => {
+  return (
+    <div className="grid-col-1">
+      <h1 className="mb-1 text-6xl font-semibold">{props.name}</h1>
+      <span className="text-3xl font-semibold italic lg:ml-5">
+        {props.description}
+      </span>
+    </div>
+  );
+};
+
+export default PageHeader;
