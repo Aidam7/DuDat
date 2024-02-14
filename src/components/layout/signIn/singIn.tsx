f334bf88c9f0095459beae39b9b06f7dcd29519a
Aidam <55688345+Aidam7@users.noreply.github.com>
2024-02-14 14:40:36 +0100

    [Feature] Now uses centered layout

--- src/components/layout/signIn/singIn.tsx
+++ src/components/layout/signIn/singIn.tsx
@@ -1,22 +1,23 @@
 import { Button } from "@nextui-org/react";
 import { signOut, signIn, useSession } from "next-auth/react";
 import React, { type FC } from "react";
+import CenteredLayout from "../centeredLayout";
 
 const SignIn: FC = () => {
   const { data: sessionData } = useSession();
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-5 text-center">
+    <CenteredLayout>
       <h1 className="text-4xl font-semibold">
         You need to sign in to access this page
       </h1>
       <Button
         color="primary"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
-        className="w-full text-2xl font-semibold"
+        className="w-[80%] text-2xl font-semibold"
       >
         {sessionData ? "Sign out" : "Sign in"}
       </Button>
-    </div>
+    </CenteredLayout>
   );
 };
 
