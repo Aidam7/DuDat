44205f70fc107ed8973e5c1402b540789fa5e1e8
Aidam <55688345+Aidam7@users.noreply.github.com>
2024-02-14 20:42:24 +0100

    [Cleanup] Now use the centeredLayout

--- src/components/layout/errorCodes/401/401.tsx
+++ src/components/layout/errorCodes/401/401.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import img401 from "public/images/errors/401.jpg";
+import CenteredLayout from "../../centeredLayout";
 
 const Code401: React.FC = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <CenteredLayout>
       <h1 className="mb-10 hidden text-9xl">401</h1>
       <p className="mb-10 hidden text-3xl">
         You are not authorized to enter this page.
       </p>
       <Image src={img401} alt="403" width={500} height={500} />
-    </div>
+    </CenteredLayout>
   );
 };
 export default Code401;
