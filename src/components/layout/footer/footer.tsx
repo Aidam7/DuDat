355e0373091ce0f0332aa5a75517f94e52b678f4
Aidam <55688345+Aidam7@users.noreply.github.com>
2024-03-18 17:30:19 +0100

    [Fix] Footer fix on mobile browsers

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -4,7 +4,7 @@ import React, { type FC } from "react";
 const Footer: FC = () => {
   return (
     <>
-      <footer className="fixed bottom-0 z-30 w-full bg-blue py-1 text-white">
+      <footer className="w-full bg-blue py-1 text-white">
         <div className="container mx-auto px-4">
           <div className="-mx-4 flex flex-wrap justify-between">
             <div className="w-full px-4 text-center sm:w-auto sm:text-left">
